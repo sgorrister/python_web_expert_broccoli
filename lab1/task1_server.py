67417506c782a262058ffdd66f7cca0d109e2763
Vadym Tsvyk <olegdychko1@gmail.com>
2023-10-12 23:35:16 +0300

    task4(added multiple clients(harno))

--- lab1/task1_server.py
+++ lab1/task1_server.py
@@ -6,20 +6,21 @@ server_socket.bind(server_address)
 server_socket.listen(1)  
 
 print("Сервер слухає на {}:{}".format(*server_address))
-client_socket, client_address = server_socket.accept()
-print("З'єднано з клієнтом {}:{}".format(*client_address))
 while True:
-    data = client_socket.recv(1024).decode('utf-8')
-    rozmir = client_socket.recv(1024).decode('utf-8')
-    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    if int(rozmir) == len(data):
-        print("Отримано від клієнта: ", data)
-        print('when:' + current_time)
-        client_socket.send(("Ви надіслали: "+data).encode('utf-8'))
-    else:
-        print("broken data")
-        client_socket.send(("Брокен дата "+data).encode('utf-8'))
-    if data == 'exit':
-        break
-client_socket.close()
+    client_socket, client_address = server_socket.accept()
+    print("З'єднано з клієнтом {}:{}".format(*client_address))
+    while True:
+        data = client_socket.recv(1024).decode('utf-8')
+        rozmir = client_socket.recv(1024).decode('utf-8')
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        if int(rozmir) == len(data):
+            print("Отримано від клієнта: ", data)
+            print('when:' + current_time)
+            client_socket.send(("Ви надіслали: "+data).encode('utf-8'))
+        else:
+            print("broken data")
+            client_socket.send(("Брокен дата "+data).encode('utf-8'))
+        if data == 'exit':
+            break
+    client_socket.close()
 server_socket.close()
