44b0efc7c00c1e3f1c6a20c2b14d201d3763dc69
Vadym Tsvyk <olegdychko1@gmail.com>
2023-11-02 23:01:19 +0200

    fix 'skills' task

--- lab3/app.py
+++ lab3/app.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 
 my_skills = ['Python', 'Flask', 'HTML', 'CSS', 'Bootstrap', 'JavaScript', 'SQL']
 
+
 @app.route('/')
 def home():
     os_info = platform.platform()
     user_agent = request.headers.get('User-Agent')
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return render_template('page1.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
+
 
 @app.route('/page1')
 def page1():
@@ -20,12 +22,14 @@ def page1():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return render_template('page1.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
 
+
 @app.route('/page2')
 def page2():
     os_info = platform.platform()
     user_agent = request.headers.get('User-Agent')
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return render_template('page2.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
+
 
 @app.route('/page3')
 def page3():
@@ -34,23 +38,20 @@ def page3():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return render_template('page3.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
 
+
+
 @app.route('/skills', methods=['GET', 'POST'])
 def display_skills():
-    if request.method == 'POST':
-        skill_name = request.form.get('skill_name')
-        for i, skill in enumerate(my_skills):
+    skill_name = request.args.get('skill_name')
+    if skill_name is not None:
+        for skill in my_skills:
             if skill_name.lower() in skill.lower():
-                return redirect(url_for('display_skill', id=i))
-        return "Навичка не знайдена."
+                return render_template('skills.html', skill=skill)
+        return render_template('skills.html', skill=None)
 
     return render_template('skills.html', my_skills=my_skills)
 
-@app.route('/skills/<int:id>')
-def display_skill(id):
-    if 0 <= id < len(my_skills):
-        return f"Навичка з id {id}: {my_skills[id]}"
-    else:
-        return "Навичка не знайдена."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
