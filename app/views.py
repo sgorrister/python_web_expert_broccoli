a7cbfdfad3b1e92737176c1b8b8d094128b535bc
Vadym Tsvyk <olegdychko1@gmail.com>
2023-11-14 17:23:58 +0200

    task 1 & task 2.1

--- app/views.py
+++ app/views.py
@@ -1,10 +1,18 @@
+import json
+
 from flask import Flask, render_template, request, redirect, url_for
+from os.path import join, dirname, realpath
+
+from app import app
 import platform
 from datetime import datetime
 
-app = Flask(__name__)
-
 my_skills = ['Python', 'Flask', 'HTML', 'CSS', 'Bootstrap', 'JavaScript', 'SQL']
+
+dataJsonPath = join(dirname(realpath(__file__)), 'users.json')
+with open(dataJsonPath, 'r+') as f:
+    users_data = json.load(f)
+
 
 @app.route('/')
 def home():
@@ -13,12 +21,14 @@ def home():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return render_template('page1.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
 
+
 @app.route('/page1')
 def page1():
     os_info = platform.platform()
     user_agent = request.headers.get('User-Agent')
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return render_template('page1.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
+
 
 @app.route('/page2')
 def page2():
@@ -27,12 +37,14 @@ def page2():
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return render_template('page2.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
 
+
 @app.route('/page3')
 def page3():
     os_info = platform.platform()
     user_agent = request.headers.get('User-Agent')
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     return render_template('page3.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
+
 
 @app.route('/skills', methods=['GET', 'POST'])
 def display_skills():
@@ -45,6 +57,7 @@ def display_skills():
 
     return render_template('skills.html', my_skills=my_skills)
 
+
 @app.route('/skills/<int:id>')
 def display_skill(id):
     if 0 <= id < len(my_skills):
@@ -52,5 +65,28 @@ def display_skill(id):
     else:
         return "Навичка не знайдена."
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users_data and users_data[username]['password'] == password:
+            return redirect(url_for('info',username=username))
+        else:
+            error = 'Невірне ім\'я користувача або пароль'
+            return render_template('login.html', error=error)
+
+    os_info = platform.platform()
+    user_agent = request.headers.get('User-Agent')
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('login.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
+
+
+@app.route('/info/<username>')
+def info(username):
+    os_info = platform.platform()
+    user_agent = request.headers.get('User-Agent')
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('info.html',username=username,os_info=os_info, user_agent=user_agent, current_time=current_time)
