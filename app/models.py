a68ee9debe47ea7b8925ecb8221d09dd0ccaea42
bubilder <olegdychko1@gmail.com>
2023-12-07 16:48:54 +0200

    made user model

--- app/models.py
+++ app/models.py
@@ -13,3 +13,13 @@ class Todo(db.Model):
 
     def __repr__(self):
         return f"Todo('{self.title}', '{self.date_created}')"
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
