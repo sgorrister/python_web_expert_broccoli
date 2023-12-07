24dda6d74a251b8a15c03a0d69d3567d25e71e36
bubilder <olegdychko1@gmail.com>
2023-12-07 16:57:01 +0200

    make migration create user table

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
