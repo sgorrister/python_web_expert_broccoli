6c62d7da822264d5461f0c17617dc1070473ccee
sgorrister <129748229+sgorrister@users.noreply.github.com>
2024-01-26 12:37:11 +0200

    peremoga at 5.00 pm (#20)

--- app/api/controller.py
+++ app/api/controller.py
@@ -1,30 +1,59 @@
-# app/api.py
 from flask import request, jsonify
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 
-from ..models import Todo, db
+from app.models import Todo, db
 from . import api_bp
+from ..models import User
+
+
+@api_bp.route('/login', methods=['POST'])
+@jwt_required(optional=True)
+def login():
+    current_user = get_jwt_identity()
+    if current_user:
+        return jsonify(message="Already logged in", user=current_user), 200
+
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=username).first()
+    if user and user.check_password(password):
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message="Invalid credentials"), 401
 
 
 @api_bp.route('/todos', methods=['GET'])
+@jwt_required()
 def get_todos():
     todos = Todo.query.all()
     todos_list = [
-        {'id': todo.id, 'title': todo.title, 'description': todo.description, 'date_created': todo.date_created} for
-        todo in todos]
+        {'id': todo.id, 'title': todo.title, 'description': todo.description, 'date_created': todo.date_created}
+        for todo in todos]
     return jsonify({'todos': todos_list})
 
 
 @api_bp.route('/todos', methods=['POST'])
+@jwt_required()
 def create_todo():
-    data = request.get_json()
-    new_todo = Todo(title=data['title'], description=data.get('description'))
-    db.session.add(new_todo)
-    db.session.commit()
-    return jsonify({'id': new_todo.id, 'title': new_todo.title, 'description': new_todo.description,
-                    'date_created': new_todo.date_created})
+    try:
+        data = request.get_json(silent=True)
+        if not data:
+            return jsonify({'error': 'Invalid JSON data'}), 400
 
+        new_todo = Todo(title=data.get('title'), description=data.get('description'))
+        db.session.add(new_todo)
+        db.session.commit()
+
+        return jsonify({'id': new_todo.id, 'title': new_todo.title, 'description': new_todo.description,
+                        'date_created': new_todo.date_created}), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @api_bp.route('/todos/<int:todo_id>', methods=['GET'])
+@jwt_required()
 def get_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
     return jsonify(
@@ -32,6 +61,7 @@ def get_todo(todo_id):
 
 
 @api_bp.route('/todos/<int:todo_id>', methods=['PUT'])
+@jwt_required()
 def update_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
     data = request.get_json()
@@ -43,6 +73,7 @@ def update_todo(todo_id):
 
 
 @api_bp.route('/todos/<int:todo_id>', methods=['DELETE'])
+@jwt_required()
 def delete_todo(todo_id):
     todo = Todo.query.get_or_404(todo_id)
     db.session.delete(todo)
--- config.py
+++ config.py
@@ -4,10 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    JWT_HEADER_NAME = 'Authorization'  # Змініть на потрібне значення
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your_jwt_secret_key'
+    JWT_SECRET_KEY = 'eyJfZnJlc2giOmZhbHNlLCJjc3JmX3Rva2VuIjoiYjY1ODExODZjMzFmZDU3MjlhMjk0ZDhkMzQyMmIwY2MwY2IzNDU1YSJ9.ZbMB4w.wXZbdLFhzefRGsU0hDkcTLndYco'
+    JWT_HEADER_TYPE = 'Bearer'  # Змініть на потрібне значення (зазвичай 'Bearer')
+    JWT_TOKEN_LOCATION = ['headers']  # Вказуємо, що токен передається в заголовку
 
 class TestConfig:
     SECRET_KEY = 'your_secret_key'
